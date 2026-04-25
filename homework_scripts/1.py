27a815ce43c867522c32621d1b3067edb94fd6c4
NewUser <newuser@example.com>
2026-04-25 17:57:10 +0300

    base stuff

---
+++ homework_scripts/1.py
@@ -0,0 +1,4 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-160m")
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-160m")
