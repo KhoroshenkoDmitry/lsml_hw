537a9cb86eb1adf735446908e02e311557016f81
NewUser <newuser@example.com>
2026-04-28 18:06:15 +0300

    Remove 1.py/useless file

--- homework_scripts/1.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-160m")
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-160m")
